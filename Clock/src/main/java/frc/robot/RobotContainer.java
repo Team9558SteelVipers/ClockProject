7a91e0cc968faa719321adcf8a98686ae4c3ad97
Moltyn3000 <mathyoojr@gmail.com>
2024-12-02 19:34:07 -0500

    StarterClock
    The starter file for the clock project

---
+++ Clock/src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,40 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+
+
+
+
+public class RobotContainer {
+
+
+  //Initialize Subsystem
+
+  //m_clockSubsystem
+
+  //Initialize both commands
+
+  //m_positionCommand
+  //m_speedCommand
+
+
+  public RobotContainer() {
+    // Configure the trigger bindings
+    configureBindings();
+  }
+
+
+  private void configureBindings() {
+
+  }
+
+  public Command getAutonomousCommand() {
+    return null;
+  }
+}
---
+++ Clock/src/main/java/frc/robot/subsystems/ClockSubsystem.java
@@ -0,0 +1,41 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class ClockSubsystem extends SubsystemBase {
+  
+  //Needs TalonFX motor
+  TalonFX motor;
+
+  public ClockSubsystem() {
+    //initialize necessary fields
+    motor = new TalonFX(0);
+  }
+
+  ///////////Methods
+
+  //SetPosition
+  //The method for setting position is:
+  //        motor.setControl(new VoltageOut(yourPositionInRotations))
+
+  //GetPosition
+
+  //SetSpeed
+
+  //GetSpeed
+
+
+
+  @Override
+  public void periodic() {
+    // No code needed here
+  }
+}
