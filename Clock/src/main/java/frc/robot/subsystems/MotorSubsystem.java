b4b2c2493ad94c94beacd2bd41fc8372abdb9a08
Ahussain008 <ahnafhussain008@gmail.com>
2025-01-24 18:12:25 -0500

    Tank Drive Code
    Code for Tank

---
+++ Clock/src/main/java/frc/robot/subsystems/MotorSubsystem.java
@@ -0,0 +1,59 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+import java.util.function.Supplier;
+
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.Constants;
+
+public class MotorSubsystem extends SubsystemBase{
+    TalonFX Motor1;
+    TalonFX Motor2;
+    TalonFX Motor3;
+    TalonFX Motor4;
+ 
+    TalonFXConfiguration pidconfig = new TalonFXConfiguration().withSlot0(new Slot0Configs().withKP(1).withKI(0).withKD(0));
+    public MotorSubsystem() {
+        Motor1 = new TalonFX(Constants.MotorPort1);
+        Motor2 = new TalonFX(Constants.MotorPort2);
+        Motor3 = new TalonFX(Constants.MotorPort3);
+        Motor4 = new TalonFX(Constants.MotorPort4);
+
+        Motor1.getConfigurator().apply(pidconfig);
+        Motor2.getConfigurator().apply(pidconfig);
+        Motor3.getConfigurator().apply(pidconfig);
+        Motor4.getConfigurator().apply(pidconfig);
+    }
+
+    public void setMotorSpeed(double speed) {
+        Motor1.set(speed);
+        Motor2.set(speed);
+        Motor3.set(-speed);
+        Motor4.set(-speed);
+    }
+
+
+    public void setMotorSpeedDyn(double speedright, double speedleft) {
+        Motor1.set(speedright);
+        Motor2.set(speedright);
+        Motor3.set(-speedleft);
+        Motor3.set(-speedleft);
+    }
+    public void stopMotors() {
+        Motor1.set(0);
+        Motor2.set(0);
+        Motor3.set(0);
+        Motor4.set(0);
+    }
+
+
+}
