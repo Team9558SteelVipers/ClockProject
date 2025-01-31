d4407c536d607a4c39c9dc2ca0b5a735e4b9b5ee
zaraaaz <zaheenzarifa@gmail.com>
2025-01-30 19:06:52 -0500

    n

--- Clock/src/main/java/frc/robot/Constants.java
+++ Clock/src/main/java/frc/robot/Constants.java
@@ -13,7 +13,13 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-  ///////////Clock Constants
+  public static class OperatorConstants {
 
-  static final int clockMotorPort = 0;
+    public static final int kDriverControllerPort = 0;}
+    public static final int MotorPortFL = 6;
+    public static final int MotorPortFR = 1;
+    public static final int MotorPortBL = 3;
+    public static final int MotorPortBR = 10;
+    public static final int coralMotorPort = 2;
+  
 }
